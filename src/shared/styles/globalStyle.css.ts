bc4b27e360c3f6649983bb032270c3ebc5673181
yeonkr <1714882@sookmyung.ac.kr>
2024-05-15 02:18:16 +0900

    feat: global, media css 파일 추가

---
+++ src/shared/styles/globalStyle.css.ts
@@ -0,0 +1,44 @@
+import { globalStyle } from '@vanilla-extract/css'
+
+globalStyle('html, body', {
+  margin: 0,
+  padding: 0,
+  height: '100%',
+})
+
+globalStyle('*, *::before, *::after', {
+  boxSizing: 'border-box',
+  WebkitFontSmoothing: 'antialiased',
+  MozOsxFontSmoothing: 'grayscale',
+})
+
+globalStyle('body', {
+  display: 'flex',
+  flexDirection: 'column',
+  margin: '0',
+  fontFamily:
+    "-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+  color: '#000',
+  background: '#fff',
+  padding: '0 16px',
+  backgroundSize: 'cover',
+  backgroundPosition: 'center center',
+  backgroundRepeat: 'no-repeat',
+})
+
+globalStyle('h1, h2, h3, h4, p', {
+  margin: 0,
+})
+
+globalStyle('main', {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  flexDirection: 'column',
+  position: 'relative',
+  width: '100%',
+  height: '100%',
+  maxWidth: '720px',
+  margin: '0 auto',
+  overflow: 'hidden',
+})
