9596c7922d79cef6bf0dd125f61e4ee6fd489156
JEONGHWANMIN <mimi0j0j@naver.com>
2024-06-02 23:49:31 +0900

    Feat: add todo

--- src/shared/ui/input/input.tsx
+++ src/shared/ui/input/input.tsx
@@ -3,6 +3,7 @@ import { inputStyle } from '@/shared/ui/input/input.css'
 
 interface InputProps extends PropsWithRef<InputHTMLAttributes<HTMLInputElement>> {}
 
+// TODO: error input style 적용하기
 const Input = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
   return <input ref={ref} className={inputStyle} {...props} />
 })
