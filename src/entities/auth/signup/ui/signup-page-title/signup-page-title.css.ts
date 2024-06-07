0a40a107f42bd3b12cd63680183d48a01a67483a
JEONGHWANMIN <mimi0j0j@naver.com>
2024-06-07 22:29:46 +0900

    Refactor: 공통 폰트 사용으로 변경

--- src/entities/auth/signup/ui/signup-page-title/signup-page-title.css.ts
+++ src/entities/auth/signup/ui/signup-page-title/signup-page-title.css.ts
@@ -1,8 +1,8 @@
 import { style } from '@vanilla-extract/css'
+import { TYPOGRAPHY } from '@/shared/styles/typography'
 
 export const signupTitleStyle = style({
   whiteSpace: 'pre-wrap',
-  fontSize: '2.4rem',
-  fontWeight: 'bold',
   marginBottom: '24px',
+  ...TYPOGRAPHY.Bold24,
 })
