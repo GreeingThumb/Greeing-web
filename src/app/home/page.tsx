3a1a214d0604768cc3981e22b2d02ee771335839
JEONGHWANMIN <mimi0j0j@naver.com>
2024-07-01 23:50:23 +0900

    Feat: 페이지 파일 생성

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,5 +1,20 @@
+import { PageLayout } from '@/widgets/page-layout'
+import { createLayoutConfig } from '@/shared/utils/layout/createLayoutConfig'
+import { HomeView } from '@/views/home'
+
 const HomePage = () => {
-  return <div />
+  const config = createLayoutConfig({
+    isBottomNav: true,
+    isHeader: true,
+    header: {
+      isLogo: true,
+    },
+  })
+  return (
+    <PageLayout config={config}>
+      <HomeView />
+    </PageLayout>
+  )
 }
 
 export default HomePage
---
+++ src/views/home/home-view.tsx
@@ -0,0 +1,5 @@
+const HomeView = () => {
+  return <div>홈</div>
+}
+
+export default HomeView
